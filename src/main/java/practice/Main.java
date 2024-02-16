da2d3a9b65457de12ecf34a3dac375b8fafb76e6
@aleksandra_khamenok <sleza.shaitana.1@gmail.com>
2024-02-16 23:46:52 +0300

    10.9 list done

--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -1,9 +1,51 @@
 package practice;
 
+import java.util.Scanner;
+
 public class Main {
     private static TodoList todoList = new TodoList();
+    private static int index = 0;
+    private static String todo = "";
 
     public static void main(String[] args) {
         // TODO: написать консольное приложение для работы со списком дел todoList
+        while (true) {
+            String input = new Scanner(System.in).nextLine();
+            String[] inputCommand = input.split("", 3);
+            switch (inputCommand[0]) {
+                case "ADD":
+                    if (inputCommand[1].matches("[0-9]+")) {
+                        index = Integer.parseInt(inputCommand[1]);
+                        todo = (inputCommand[2]);
+                        todoList.add(index, todo);
+
+                    } else {
+                        todo = inputCommand[1] + " " + inputCommand[2];
+                        todoList.add(todo);
+                    }
+                    break;
+                case "EDIT":
+                    if (inputCommand[1].matches("[0-9]+")) {
+                        index = Integer.parseInt(inputCommand[1]);
+                        todo = inputCommand[2];
+                        todoList.edit(index, todo);
+                    }
+                    break;
+
+                case "DELETE":
+                    if (inputCommand[1].matches("[0-9] + ")) {
+                        index = Integer.parseInt(inputCommand[1]);
+                        todoList.delete(index);
+                    }
+                    break;
+                case "LIST":
+                    todoList.getTodos();
+                    for (int i = 0; i < todoList.list.size(); i++) {
+                        System.out.println(i + "-" + todoList.list.get(i));
+                    }
+                    break;
+            }
+        }
     }
 }
+
--- src/main/java/practice/TodoList.java
+++ src/main/java/practice/TodoList.java
@@ -3,29 +3,52 @@ package practice;
 import java.util.ArrayList;
 
 public class TodoList {
+    public ArrayList<String> list = new ArrayList<>();
 
     public void add(String todo) {
         // TODO: добавьте переданное дело в конец списка
+        list.add(todo);
+        System.out.println("Добавлено дело " + '"' + todo + '"');
+
     }
 
     public void add(int index, String todo) {
         // TODO: добавьте дело на указаный индекс,
         //  проверьте возможность добавления
+        if (index >= 0 && index <= list.size()) {
+            list.add(index, todo);
+            System.out.println("Добавлено дело" + '"' + todo + '"' + " на указанный номер " + index);
+        } else {
+            list.add(todo);
+            System.out.println("Дело с таким номером не существует");
+        }
     }
 
     public void edit(int index, String todo) {
         // TODO: заменить дело на index переданным todo индекс,
         //  проверьте возможность изменения
+        if (index <= list.size() -1) {
+            System.out.println("Дело" + '"' + list.get(index) + '"' + "заменено на " + '"' + todo + '"');
+            list.set(index, todo);
+        } else {
+            System.out.println("Дело с таким номером не существует");
+        }
     }
 
     public void delete(int index) {
         // TODO: удалить дело находящееся по переданному индексу,
         //  проверьте возможность удаления дела
+        if (index <= list.size() -1){
+            System.out.println("Дело" + '"' + list.get(index) + " удалено ");
+            list.remove(index);
+        } else {
+            System.out.println("Дело с таким номером не сущесивует");
+        }
     }
 
     public ArrayList<String> getTodos() {
         // TODO: вернуть список дел
-        return new ArrayList<>();
+        return list;
     }
 
 }
